4018d632707b25cdfb197d3eacc38a4de03a86d2
Snail Mail <>
2025-11-18 17:58:16 -0600

    add dead zome taper

--- platform/simple_wah/taper.h
+++ platform/simple_wah/taper.h
@@ -3,3 +3,10 @@ inline float inv_log_taper(float x) { return x < 0.5f ? x * 1.8f : x * 0.2f + 0.
 
 // log taper
 inline float log_taper(float x) { return x < 0.5f ? x * 0.2f : x * 1.8f - 0.8f; }
+
+// dead zone taper
+inline float dead_zone_taper(float x) { 
+    if(x < 0.1) return 0.0f;
+    else if(x > 0.9) return 1.0f;
+    else return 1.25f * x - 0.125f; 
+ }
