2a53c9308b38c54c894ed84db0b8c46aa35285c2
Snail Mail <>
2025-11-29 20:23:24 -0600

    streamline two filter wah

--- platform/two_filter_wah/main.cpp
+++ platform/two_filter_wah/main.cpp
@@ -6,41 +6,35 @@ using namespace daisy;
 using namespace daisy::seed;
 using namespace daisysp;
 
-
 DaisySeed hw;
+Svf filter1, filter2;
 
 constexpr float Filter1Min = 200.0f;
 constexpr float Filter1Max = 1600.0f;
 constexpr float Filter2Min = 450.0f;
 constexpr float Filter2Max = 3500.0f;
 constexpr float QMin = 0.5f;
-constexpr float QMax = 1.2f;
+constexpr float QMax = 1.0f;
+constexpr float ChangeThreshold = 0.02f;
 
-inline float range(float min, float max, float value) { return min + (value * (max - min)); }
-
-bool filter_mode; // false - low pass, true - babd pass
-bool wah_mode;    // false - single filter, true - two (formant) filters
-                  
-Svf filter1, filter2;
-
-float filter1_freq = range(Filter1Min, Filter1Max, 0.5f);
-float filter2_freq = range(Filter2Min, Filter2Max, 0.5f);
 float pot1, pot2;
 
 void ProcessAnalogControls() {
-    float pot1v = hw.adc.GetFloat(0);
-    float pot2v = hw.adc.GetFloat(1);
+    const float pot1v = hw.adc.GetFloat(0);
+    const float pot2v = hw.adc.GetFloat(1);
 
-    if(fabs(pot1v - pot1) > 0.05f) {
-        // 350 Hz - 2.2 kHz
-        filter1_freq = range(Filter1Min, Filter1Max, pot1v);
+    if(fabs(pot1v - pot1) > ChangeThreshold) {
+        const float filter1_freq = fmap(Filter1Min, Filter1Max, pot1v);
+        const float filter2_freq = fmap(Filter2Min, Filter2Max, 1.0f - pot1v);
         filter1.SetFreq(filter1_freq);
+        filter2.SetFreq(filter2_freq);
         pot1 = pot1v;
     }
  
-    if(fabs(pot2v - pot2) > 0.05f) {
-        float q = range(QMin, QMax, pot2v);
+    if(fabs(pot2v - pot2) > ChangeThreshold) {
+        float q = fmap(QMin, QMax, pot2v);
         filter1.SetRes(q);
+        filter2.SetRes(q);
         pot2 = pot2v;
     }    
 }
@@ -57,19 +51,18 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
         filter2.Process(input);
 
         // TODO: Also try filter2.Process(filter1_out);
-        float filter1_out = filter_mode ? filter1.Band() : filter1.Low();
+        float filter1_out = filter1.Band();
         float filter2_out = filter2.Band();
 
-        if(wah_mode)
-          out[0][i] = out[1][i] = filter1_out + filter2_out;
-        else 
-          out[0][i] = out[1][i] = filter1_out; 
+        out[0][i] = out[1][i] = filter1_out + filter2_out;
     }
 }
 
 void init() 
 {
     const int num_adc_channels = 2;
+    const float filter1_freq = fmap(Filter1Min, Filter1Max, .5f);
+    const float filter2_freq = fmap(Filter2Min, Filter2Max, .5f);
 
     hw.Init();
     hw.SetAudioBlockSize(4);
@@ -80,7 +73,7 @@ void init()
     hw.adc.Init(my_adc_config, num_adc_channels);
     hw.adc.Start();
 
-    float sample_rate = hw.AudioSampleRate();
+    float sample_rate = hw.AudioSampleRate();        
 
     filter1.Init(sample_rate);
     filter1.SetFreq(filter1_freq);
@@ -97,14 +90,9 @@ void init()
 
 int main(void)
 {
-    pot1 = pot2 = 0;
-    filter_mode = true;     // false - low pass, true - babd pass
-    wah_mode = false;       // false - single filter, true - two (formant) filters
-    
+    pot1 = pot2 = 0;    
     init();
 
-    while(1) {
-        hw.SetLed(pot1 > 0.5f || pot2 > 0.5f);
-        System::Delay(5);
+    while(true) {
     }
 }
